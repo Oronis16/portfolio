278500dc4e8893ef96798d0dab2cd5f9b8cc29a5
Krisztina Andreyka <andreyka.kriszti@gmail.com>
2021-05-19 21:01:19 +0200

    extended with ight and dark mode

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,27 @@ import {
   Flex,
   Spacer,
   Stack,
-  Switch,
+  Square,
+  Circle,
+  UnorderedList,
+  ListItem,
+  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 
 export default function Home() {
+  const { toggleColorMode } = useColorMode();
+
+  const bg = useColorModeValue("white", "brandDark.primary");
+  const bg2 = useColorModeValue("brandLight.secondary", "brandDark.secondary");
+  const circleBG = useColorModeValue("brandDark.primary", "white");
+  const highlightColor = useColorModeValue(
+    "brandLight.secondaryText",
+    "brandDark.secondaryText"
+  );
+
   return (
-    <>
+    <Box bg={bg}>
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
@@ -27,14 +42,19 @@ export default function Home() {
             fontSize="36px"
             fontWeight="light"
             fontFamily="poppins"
+            letterSpacing="10px"
           >
             Andreyka Krisztina
           </Box>
         </Box>
         <Spacer />
-        <Stack align="center" direction="row" mr="15px">
-          <Switch colorScheme="blackAlpha" size="sm" />
-        </Stack>
+        <Circle
+          mt="15px"
+          mr="10px"
+          size="20px"
+          bg={circleBG}
+          onClick={toggleColorMode}
+        ></Circle>
         <Box>
           <Box
             as="button"
@@ -61,10 +81,23 @@ export default function Home() {
           <Stack ml="150px">
             <Text fontFamily="Poppins" fontSize="65px" fontWeight="semibold">
               Building modern apps for todays and for the
-              <Text display="inline-block" color="brand.500">
+              <Text display="inline-block" color={highlightColor}>
                 &nbsp;future
               </Text>
             </Text>
+            <Flex>
+              <Square
+                mt="13px"
+                mr="15px"
+                w="69px"
+                h="7px"
+                bg={highlightColor}
+              ></Square>
+              <Text fontFamily="poppins" fontSize="24px" fontWeight="regular">
+                Hi, I'm Kriszti 👋 &nbsp;Aspiring Junior Software Developerfrom
+                the heart of Europe.
+              </Text>
+            </Flex>
           </Stack>
           <Spacer />
           <Box
@@ -78,7 +111,112 @@ export default function Home() {
             <Image src="Female_Spanish.png" />
           </Box>
         </Flex>
+        <Box mt="100px" bg={bg2} h="774px">
+          <Flex>
+            <Square
+              ml="150px"
+              mt="105px"
+              mr="15px"
+              w="69px"
+              h="7px"
+              bg={highlightColor}
+            ></Square>
+            <Text
+              mt="70px"
+              fontFamily="poppins"
+              fontSize="48px"
+              fontWeight="thin"
+              letterSpacing="5px"
+            >
+              SKILLS
+            </Text>
+          </Flex>
+          <Flex
+            mt="50px"
+            ml="330px"
+            fontFamily="poppins"
+            fontSize="30px"
+            fontWeight="200"
+            letterSpacing="5px"
+          >
+            <Text>FRONTEND</Text>
+            <Text letterSpaing="5px" ml="450px">
+              BACKEND
+            </Text>
+          </Flex>
+          <Flex>
+            <Box
+              ml="300px"
+              mt="30px"
+              fontFamily="Montserrat"
+              fontSize="regular"
+            >
+              <UnorderedList fontSize="30px">
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>JavaScript</Text>
+                </ListItem>
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>HTML</Text>
+                </ListItem>
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>CSS</Text>
+                </ListItem>
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>React.js</Text>
+                </ListItem>
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>Next.js</Text>
+                </ListItem>
+                <ListItem color={highlightColor}>
+                  <Text color={circleBG}>Figma</Text>
+                </ListItem>
+              </UnorderedList>
+            </Box>
+            <Box
+              ml="460px"
+              mt="30px"
+              fontFamily="Montserrat"
+              fontSize="regular"
+            >
+              <UnorderedList fontSize="30px">
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>Node.js</Text>
+                </ListItem>
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>Express</Text>
+                </ListItem>
+                <ListItem mb="25px" color={highlightColor}>
+                  <Text color={circleBG}>PostgreSQL</Text>
+                </ListItem>
+                <ListItem color={highlightColor}>
+                  <Text color={circleBG}>REST</Text>
+                </ListItem>
+              </UnorderedList>
+            </Box>
+          </Flex>
+        </Box>
+        <Box>
+          <Flex>
+            <Square
+              ml="150px"
+              mt="105px"
+              mr="15px"
+              w="69px"
+              h="7px"
+              bg={highlightColor}
+            ></Square>
+            <Text
+              mt="70px"
+              fontFamily="poppins"
+              fontSize="48px"
+              fontWeight="200"
+              letterSpacing="5px"
+            >
+              PROJECTS
+            </Text>
+          </Flex>
+        </Box>
       </Box>
-    </>
+    </Box>
   );
 }
--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,24 @@
 // 1. Import `extendTheme`
-import { extendTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
   colors: {
-    brand: {
-      500: "#5C1BE5"
+    brandLight: {
+      500: "#5C1BE5",
+      600: "#5C1BE5",
+      700: "#635F5F",
+      primary: "#FFFFFF",
+      secondary: "#E5E5E5",
+      primaryText: "#000000",
+      secondaryText: "#5C1BE5",
+    },
+    brandDark: {
+      primary: "#242323",
+      secondary: "#635F5F",
+      primaryText: "#FFFFFF",
+      secondaryText: "#B49BEB",
     },
   },
-})
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+});
